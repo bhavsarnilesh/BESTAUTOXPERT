19b5cf3692ff908e0f441f41847ee9c3492450f0
Nilesh Bhavsar <68153245+bhavsarnilesh@users.noreply.github.com>
2020-10-17 19:16:05 +0530

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './public/home/home.component';
+import { GallaryComponent } from './public/gallary/gallary.component';
+import { BlogComponent } from './public/blog/blog.component';
+import { EventsComponent } from './public/events/events.component';
+import { ContactComponent } from './public/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', pathMatch: 'full', component: HomeComponent},
+  { path: 'gallary', pathMatch: 'full', component: GallaryComponent},
+  { path: 'blog', pathMatch: 'full', component: BlogComponent},
+  { path: 'events', pathMatch: 'full', component: EventsComponent},
+  { path: 'contact', pathMatch: 'full', component: ContactComponent}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './public/home/home.component';
+import { GallaryComponent } from './public/gallary/gallary.component';
+import { BlogComponent } from './public/blog/blog.component';
+import { EventsComponent } from './public/events/events.component';
+import { ContactComponent } from './public/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GallaryComponent,
+    BlogComponent,
+    EventsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
